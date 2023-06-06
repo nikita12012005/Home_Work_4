a37efdd5e38bde1a86053309c731a6f00150b161
Nikita <kruknikitron@gmail.com>
2023-06-06 22:46:48 +0300

    exercise finish

---
+++ test/5hw/home work_11/1_exrcise.py
@@ -0,0 +1,11 @@
+class Apple:
+    def __init__(self, name: str, employees: int,country: str):
+        self.name= name
+        self.employees= employees
+        self.country= country
+
+if __name__=='__main__':
+    company= Apple('Apple', 164000, 'USA')
+print(f"""Name: {company.name}
+Employees: {company.employees}
+Country: {company.country}""")
