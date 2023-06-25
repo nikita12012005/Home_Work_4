ece73c5ce928016b18d54c1f6d67cd9fcb7e8e46
Nikita <kruknikitron@gmail.com>
2023-06-25 20:07:31 +0300

    finish exercise nove

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import re
 #1_exercise
-line = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
+string = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
 
-for sssss in line.split('=sssss'):
-    for equals in sssss.replace('=', ':'):
-        for ampersand in equals.replace('&', ','):
-            print(ampersand)
+string= string.strip()
+params =string.split('&')
+pairs= {}
 
+for param in params:
+    if param:
+        key, value = param.split('=', maxsplit=1)
+        pairs[key]= value
+
+print(pairs)
 #2_exercise
-def camel_to_snake(variable_name):
-    result = ''
-    for i, char in enumerate(variable_name):
-        if char.isupper() and i > 0:
-            result += '_'
-        result += char.lower()
-    return result
+camel_case_names= ["FirstItem", "FriendsList", "MyTuple"]
+snake_case_names= []
 
-camel_case_list = ["FirstItem", "FriendsList", "MyTuple"]
-snake_case_list = [camel_to_snake(variable_name) for variable_name in camel_case_list]
-
-print(snake_case_list)
+for name in camel_case_names:
+    some= re.sub(r"([A-Z][a-z]+)([A-Z][a-z]+)", r"\1_\2", name).lower()
+    print(some)
 
 #3_exercise
-text = "'''The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was" \
+text = """The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was" \
       " launched into low Earth orbit in 1990 and remains in operation.... It was not the first space telescope," \
       " but it is one of the largest and most versatile, renowned both as a vital research tool and as a public " \
       "relations boon for astronomy. The Hubble telescope is named after astronomer Edwin Hubble and is one of " \
@@ -31,11 +30,9 @@ text = "'''The Hubble.Space.Telescope (often referred to as HST or Hubble) is a 
       " commission headed by Lew Allen, director of the Jet Propulsion Laboratory, was established to determine how " \
       "the error could have arisen. The Allen Commission found that a reflective null corrector," \
       " a testing device used to achieve a properly shaped non-spherical mirror, had been incorrectly" \
-      " assembled—one lens was out of position by 1.3 mm (0.051 in).'''"
+      " assembled—one lens was out of position by 1.3 mm (0.051 in)."""
 
-replaced_text = text.replace("....", ".") \
-                   .replace(".A", ". A") \
-                   .replace("Hubble.Space.Telescope", "Hubble Space Telescope")
+replaced_text = re.findall(r"^[A-Z].+\.$", text)
 
 print(replaced_text)
 
@@ -72,8 +69,10 @@ index = '''
 </html>
 '''
 
-pattern = r'<div id="([^"]+)">\s*<a href="([^"]+)">\s*([^<]+)\s*</a>\s*</div>'
+pattern = r'<div id="([^"]+)">\s*<a href="([^"]+)">\s*([^<]+)\s*</a>\s'
 
 result = re.findall(pattern, index)
 
-print(result)
+cleaned_result = [(group1.strip(), group2.strip(), group3.strip()) for group1, group2, group3 in result]
+
+print(cleaned_result)
