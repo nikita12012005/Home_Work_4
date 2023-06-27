498714503aea1cfc9ef84b332f504f66a003f22b
Nikita <kruknikitron@gmail.com>
2023-06-27 18:25:35 +0300

    finish exercise nove !

--- test/5hw/home work_11/1_exrcise.py
+++ test/5hw/home work_11/1_exrcise.py
@@ -1,12 +1,54 @@
-class Apple:
-    def __init__(self, name: str, employees: int,country: str):
-        self.name= name
-        self.employees= employees
-        self.country= country
+class Company:
+    def __init__(self, name, industry, headquarters, founder, year_founded):
+        self.name = name
+        self.industry = industry
+        self.headquarters = headquarters
+        self.founder = founder
+        self.year_founded = year_founded
 
-if __name__=='__main__':
-    company= Apple('Apple', 164000, 'USA')
-print(f"""Name: {company.name}
-Employees: {company.employees}
-Country: {company.country}""")
+    def get_name(self):
+        return self.name
+
+    def get_industry(self):
+        return self.industry
+
+    def get_headquarters(self):
+        return self.headquarters
+
+    def get_founder(self):
+        return self.founder
+
+    def get_year_founded(self):
+        return self.year_founded
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_industry(self, industry):
+        self.industry = industry
+
+    def set_headquarters(self, headquarters):
+        self.headquarters = headquarters
+
+    def set_founder(self, founder):
+        self.founder = founder
+
+    def set_year_founded(self, year_founded):
+        self.year_founded = year_founded
+
+
+toshiba = Company("Toshiba", "Electronics", "Tokyo, Japan", "Hisashige Tanaka", 1875)
+apple = Company("Apple", "Technology", "Cupertino, California, United States", "Steve Jobs, Steve Wozniak, Ronald Wayne", 1976)
+
+print(toshiba.get_name())
+print(toshiba.get_industry())
+print(toshiba.get_headquarters())
+print(toshiba.get_founder())
+print(toshiba.get_year_founded())
+
+print(apple.get_name())
+print(apple.get_industry())
+print(apple.get_headquarters())
+print(apple.get_founder())
+print(apple.get_year_founded())
 
--- test/5hw/home work_11/2_exercise.py
+++ test/5hw/home work_11/2_exercise.py
@@ -1,13 +1,55 @@
 class Worker:
-    def __init__(self, name: str, employee_id: int,position: str, department: str):
+    def __init__(self, name: str, age: int, position: str, department: str, salary: float):
         self.name = name
-        self.employee_id = employee_id
+        self.age = age
         self.position = position
         self.department = department
+        self.salary = salary
 
-if __name__=='__main__':
-    company= Worker('Dima', 164000, 'Senior Vice Presidents', 'Department of Justice')
-print(f"""Name: {company.name}
-Employee_id: {company.employee_id}
-Position: {company.position}
-Department: {company.department}""")
+    def get_name(self) -> str:
+        return self.name
+
+    def get_age(self) -> int:
+        return self.age
+
+    def get_position(self) -> str:
+        return self.position
+
+    def get_department(self) -> str:
+        return self.department
+
+    def get_salary(self) -> float:
+        return self.salary
+
+    def set_name(self, name: str):
+        self.name = name
+
+    def set_age(self, age: int):
+        self.age = age
+
+    def set_position(self, position: str):
+        self.position = position
+
+    def set_department(self, department: str):
+        self.department = department
+
+    def set_salary(self, salary: float):
+        self.salary = salary
+
+worker = Worker("John Smith", 30, "Software Engineer", "Engineering", 5000.00)
+
+
+print(worker.get_name())
+print(worker.get_age())
+print(worker.get_position())
+print(worker.get_department())
+print(worker.get_salary())
+
+
+worker.set_position("Senior Software Engineer")
+worker.set_salary(6000.00)
+
+
+print(worker.get_position())
+print(worker.get_salary())
+
